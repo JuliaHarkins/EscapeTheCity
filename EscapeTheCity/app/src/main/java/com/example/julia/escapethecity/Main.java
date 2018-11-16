d7c5ed1121d1cf917a4c4b0a2e17cebed4d225d7
Unknown <shaunsupra@googlemail.com>
2018-11-16 10:34:29 +0000

    Test

--- EscapeTheCity/app/src/main/java/com/example/julia/escapethecity/Main.java
+++ EscapeTheCity/app/src/main/java/com/example/julia/escapethecity/Main.java
@@ -35,6 +35,7 @@ public class Main extends AppCompatActivity {
         return true;
     }
 
+    // This is a comment to test GitKraken
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
